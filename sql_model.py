869858f9b0e68d06b1f15d70b27c8dfe63330251
calmcoconut <diaz.alejandro772@gmail.com>
2021-06-28 17:46:16 +0900

    word database created, simple selection algo implemented

--- sql_model.py
+++ sql_model.py
@@ -1,9 +1,13 @@
 import sqlite3
 from sqlite3 import Error
+from random import seed, randint
+from time import time
 from os import path
 
 class Database:
+	seed(time())
 	__DB_LOCATION = path.join(path.realpath('.'),"data","typing.db")
+	__CSV_LOCATION = path.join(path.realpath('.'),"data","words.csv")
 	__create_word_table = """CREATE TABLE IF NOT EXISTS words (
 		id integer PRIMARY KEY,
 		name text NOT NULL
@@ -15,6 +19,7 @@ class Database:
 		self.__conn = self.__create_connection(self.__DB_LOCATION)
 		self.__cursor = self.__conn.cursor()
 		self.__create_table()
+		self.__init_words()
 	
 	def __create_connection(self, db_file):
 		"""connect to db path"""
@@ -26,9 +31,21 @@ class Database:
 			print(e)
 		finally:
 			return conn
+	def select_all_from_table(self, table):
+		sql_command = """SELECT * FROM {}""".format(table)
+		self.__cursor.execute(sql_command)
+		return self.__cursor.fetchall()
+	
+	def select_random_word(self):
+		return self.select_by_id(self.WORDS_TABLE, randint(1,1000))
+
 	def select_by_id(self, table, id):
-		sql_command = """SELECT * FROM {} WHERE {};"""
-		pass
+		"""
+		fetches first matching id
+		"""
+		sql_command = """SELECT * FROM {} WHERE id=?""".format(table)
+		rows = self.__cursor.execute(sql_command,(id,)).fetchone()
+		return rows
 
 	def select_by_custom(self, table, condition):
 		pass
@@ -43,6 +60,17 @@ class Database:
 			)
 		except Error as e:
 			print(e)
+
+	def __init_words(self):
+		words = self.select_by_id(self.WORDS_TABLE, 1)
+		if words is None:
+			# parse csv
+			with open(self.__CSV_LOCATION,'r') as reader:
+				for id, line in enumerate(reader.readlines(), start=0):
+					line = line.strip('\n\r')
+					self.insert_data(self.WORDS_TABLE, self.WORDS_TABLE_COLS, (id,line))
+					
+
 	def insert_data(self, table, columns,data):
 		"""inserts data into a table. will error if table does not exist
 		:param table string TABLE NAME:
@@ -52,12 +80,13 @@ class Database:
 
 		col_num = "?"+(",?"*columns.count(","))
 		sql_command = """INSERT INTO {}({}) VALUES({})""".format(table, columns,col_num)
-		for row in data:
-			self.__cursor.execute(sql_command, row)
+		self.__cursor.execute(sql_command, data)
 		self.__conn.commit()
 		
 
 
 if __name__=="__main__":
+	import random
 	db = Database()
-	db.insert_data(db.WORDS_TABLE, db.WORDS_TABLE_COLS, [(1,"hello")])
+	random_word = db.select_random_word()[1]
+	db.close()
